a7f66b7b16a9fef31009d88d107d1005757e67d8
thanormsaksudsee <s6506022420037@email.kmutnb.ac.th>
2023-08-09 13:00:41 +0700

    sddq

--- chatclient.py
+++ chatclient.py
@@ -1,7 +1,9 @@
 from socket import *
+from threading import Thread
 
 HOST = '10.60.131.140'  # เปลี่ยนเป็น IP ของเครื่อง server ที่เชื่อมต่อกับ
 PORT = 5000
+BUFFER_SIZE = 4096  # กำหนดขนาดของ buffer
 
 client = socket(AF_INET, SOCK_STREAM)
 client.connect((HOST, PORT))
@@ -9,8 +11,16 @@ client.connect((HOST, PORT))
 name = input("Enter your name: ")
 client.send(str.encode(name))
 
+def receive_messages():
+    while True:
+        message = bytes.decode(client.recv(BUFFER_SIZE))
+        print(message)
+
+receive_thread = Thread(target=receive_messages)
+receive_thread.start()
+
 while True:
-    message = input("Enter your message: ")
+    message = input()
     if message.lower() == "quit":
         client.send(str.encode(message))
         print("Goodbye!")
