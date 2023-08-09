c567e4d9770e6cb0d4a9be48ab0ec57561d52bd3
thanormsaksudsee <s6506022420037@email.kmutnb.ac.th>
2023-08-10 00:44:51 +0700

    bg

--- Client.py
+++ Client.py
@@ -1,3 +1,56 @@
+# import socket
+# import threading
+
+# from time import ctime
+
+# COLOR_RESET = '\033[0m'
+# COLOR_BLUE = '\033[91m'  # Blue color
+# COLOR_GREEN = '\033[92m'  # Green color
+
+# HOST = '192.168.255.1'
+# PORT = '12345'
+# nickname = input("User Name: ")
+
+
+# # Connecting To Server
+# client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# print(f"Connecting to {HOST}:{PORT}...")
+# client.connect(('192.168.255.1', 12345))                            #Instead of connecting, we'r binding to the host's port(Port Number is Confidential)
+
+
+# def receive():
+#     while True:
+#         try:
+
+#             message = client.recv(1024).decode('ascii')  
+
+#             if message == 'NICK':                           
+#                 client.send(nickname.encode('ascii'))    
+#             else:
+#                 print(COLOR_GREEN + message + COLOR_RESET)
+#         except:
+#             print(ctime() + ' ' + COLOR_BLUE + nickname + ' Disconnect!' + COLOR_RESET)
+#             client.close()
+#             break
+
+# # Sending Messages To Server
+# def write():
+#     while True:
+#         message = ctime() +' User '+ '{}: {}'.format(nickname, input(''))
+#         client.send(message.encode('ascii'))
+#         if message.endswith(": quit"):
+#             client.close()
+#             break
+
+
+# # Starting Threads For Listening And Writing
+# receive_thread = threading.Thread(target=receive)
+# receive_thread.start()
+
+# write_thread = threading.Thread(target=write)
+# write_thread.start() 
+
+
 import socket
 import threading
 
@@ -7,9 +60,19 @@ COLOR_RESET = '\033[0m'
 COLOR_BLUE = '\033[91m'  # Blue color
 COLOR_GREEN = '\033[92m'  # Green color
 
+
 HOST = '192.168.255.1'
 PORT = '12345'
+
+print('################## Welcome to the BigWorld ##################')
+print('#################### Join to the Lobby ####################')
+print('################## Enter your User Name ##################')
 nickname = input("User Name: ")
+
+print('##Select room##')
+print('Select room 1 Enter 1')
+print('Select room 2 Enter 2')
+selectRoom = input('Select Room: ')
 
 
 # Connecting To Server
--- Server.py
+++ Server.py
@@ -1,83 +1,265 @@
+# import socket
+# import threading
+# from time import ctime
+
+# host = '192.168.255.1'                                                 
+# port = 12345                                                       
+# server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)         
+# server.bind((host, port))                                          
+# server.listen()                                                   
+
+
+# clients = [] 
+# nicknames = []
+
+# def broadcast(message): 
+#     for client in clients:
+#         client.send(message)                                       
+
+ 
+# def handle(client):
+#     try:
+#         while True:
+#             message = client.recv(1024)
+#             if message.decode('ascii') == "quit":
+#                 client.send('You have disconnected.'.encode('ascii'))
+#                 client.close()
+#                 print('{} has disconnected.'.format(nickname))
+#                 break
+#             else:
+#                 broadcast(message)
+#     except:
+#         index = clients.index(client)
+#         clients.remove(client)
+#         nickname = nicknames[index]
+#         broadcast('{} Disconnect!'.format(nickname).encode('ascii'))
+#         nicknames.remove(nickname)
+#         client.close()
+#         print('{} has Disconnected .'.format(nickname) +' '+ctime())  
+
+
+# def receive():
+#     while True:
+#         client, address = server.accept()                          
+#         print("Connected with {}".format(str(address)+ ' '+ctime()))   
+
+#         client.send('NICK'.encode('ascii'))                       
+#         nickname = client.recv(1024).decode('ascii')                
+#         nicknames.append(nickname)
+#         clients.append(client)
+
+
+#         print("Name is {}".format(nickname))
+#         #broadcasting
+#         broadcast("{} joined!".format(nickname).encode('ascii'))   
+#         client.send('Connected to server!'.encode('ascii'))        
+
+#         thread = threading.Thread(target=handle, args=(client,))   
+#         thread.start()                                             
+
+# print("Server is started..............,:) ")
+# receive()
+
+
+
+
+def room1():
+    import socket
+    import threading
+    from time import ctime
+
+    host = '192.168.255.1'                                                 
+    port = 12345                                                       
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)         
+    server.bind((host, port))                                          
+    server.listen()                                                   
+    print("Welcome to Room 1")
+
+    clients = [] 
+    nicknames = []
+
+    def broadcast(message): 
+        for client in clients:
+            client.send(message)                                       
+
+    
+    def handle(client):
+        try:
+            while True:
+                message = client.recv(1024)
+                if message.decode('ascii') == "quit":
+                    client.send('You have disconnected.'.encode('ascii'))
+                    client.close()
+                    print('{} has disconnected.'.format(nickname))
+                    break
+                else:
+                    broadcast(message)
+        except:
+            index = clients.index(client)
+            clients.remove(client)
+            nickname = nicknames[index]
+            broadcast('{} Disconnect!'.format(nickname).encode('ascii'))
+            nicknames.remove(nickname)
+            client.close()
+            print('{} has Disconnected .'.format(nickname) +' '+ctime())  
+
+
+    def receive():
+        while True:
+            client, address = server.accept()                          
+            print("Connected with {}".format(str(address)+ ' '+ctime()))   
+
+            client.send('NICK'.encode('ascii'))                       
+            nickname = client.recv(1024).decode('ascii')                
+            nicknames.append(nickname)
+            clients.append(client)
+
+
+            print("Name is {}".format(nickname))
+            #broadcasting
+            broadcast("{} joined!".format(nickname).encode('ascii'))   
+            client.send('Connected to server!'.encode('ascii'))        
+
+            thread = threading.Thread(target=handle, args=(client,))   
+            thread.start()                                             
+
+    print("Server is started..............,:) ")
+    receive()
+
+
+
+def room2():
+    import socket
+    import threading
+    from time import ctime
+    
+    print("Welcome to Room 2")
+
+    host = '192.168.255.1'                                                 
+    port = 12345                                                       
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)         
+    server.bind((host, port))                                          
+    server.listen()                                                   
+
+
+    clients = [] 
+    nicknames = []
+
+    def broadcast(message): 
+        for client in clients:
+            client.send(message)                                       
+
+    
+    def handle(client):
+        try:
+            while True:
+                message = client.recv(1024)
+                if message.decode('ascii') == "quit":
+                    client.send('You have disconnected.'.encode('ascii'))
+                    client.close()
+                    print('{} has disconnected.'.format(nickname))
+                    break
+                else:
+                    broadcast(message)
+        except:
+            index = clients.index(client)
+            clients.remove(client)
+            nickname = nicknames[index]
+            broadcast('{} Disconnect!'.format(nickname).encode('ascii'))
+            nicknames.remove(nickname)
+            client.close()
+            print('{} has Disconnected .'.format(nickname) +' '+ctime())  
+
+
+    def receive():
+        while True:
+            client, address = server.accept()                          
+            print("Connected with {}".format(str(address)+ ' '+ctime()))   
+
+            client.send('NICK'.encode('ascii'))                       
+            nickname = client.recv(1024).decode('ascii')                
+            nicknames.append(nickname)
+            clients.append(client)
+
+
+            print("Name is {}".format(nickname))
+            #broadcasting
+            broadcast("{} joined!".format(nickname).encode('ascii'))   
+            client.send('Connected to server!'.encode('ascii'))        
+
+            thread = threading.Thread(target=handle, args=(client,))   
+            thread.start()                                             
+
+    print("Server is started..............,:) ")
+    receive()
+
 import socket
 import threading
-
-# Connection Data  
-#Port is confidential thing
-host = '192.168.255.1'                                                 #IP of your host
-port = 12345                                                       #Dont take reserved ports
-
-# Starting Server
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)         #Internet socket
-server.bind((host, port))                                          #Server is binded to local host and a port  
-server.listen()                                                    #Server is set to listining mode
-
-
 from time import ctime
 
-# Lists For Clients and Their Nicknames
+host = '192.168.255.1'                                                 
+port = 12345                                                       
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)         
+server.bind((host, port))                                          
+server.listen()                                                   
+
+
 clients = [] 
 nicknames = []
 
-# Sending Messages To All Connected Clients
 def broadcast(message): 
     for client in clients:
-        client.send(message)                                        #Getting all the clients and sending msg that new one joined to the admin
+        client.send(message)                                       
 
-# Handling Messages From Clients
-# ...
-
+ 
 def handle(client):
     try:
-        # ... (previous code)
         while True:
-            # Broadcasting Messages
             message = client.recv(1024)
             if message.decode('ascii') == "quit":
-                # ... (previous code)
                 client.send('You have disconnected.'.encode('ascii'))
                 client.close()
-                print('{} has disconnected.'.format(nickname))  # Print disconnected message
+                print('{} has disconnected.'.format(nickname))
                 break
             else:
                 broadcast(message)
     except:
-        # Removing And Closing Clients
         index = clients.index(client)
         clients.remove(client)
         nickname = nicknames[index]
         broadcast('{} Disconnect!'.format(nickname).encode('ascii'))
         nicknames.remove(nickname)
         client.close()
-        print('{} has disconnected .'.format(nickname) +' '+ctime())  # Print error disconnect message
-
-# ...
-    
+        print('{} has Disconnected .'.format(nickname) +' '+ctime())  
 
 
 def receive():
     while True:
-        # Accept Connection
-        client, address = server.accept()                          #In server IP wil login so different IP will login with same port on the server IP
+        client, address = server.accept()                          
         print("Connected with {}".format(str(address)+ ' '+ctime()))   
 
-        #Getting nickname by asking --- code word sent the client and the client side accepts the encoded code which is NICK here and send backs the message that he can join
-        #Request And Store Nickname
-
-        client.send('NICK'.encode('ascii'))                        #Sending in encoded form 
+        client.send('NICK'.encode('ascii'))                       
         nickname = client.recv(1024).decode('ascii')                
         nicknames.append(nickname)
         clients.append(client)
 
-        # Print And Broadcast Nickname  
-        #pritning
+
         print("Name is {}".format(nickname))
         #broadcasting
-        broadcast("{} joined!".format(nickname).encode('ascii'))   #So that every client will know new connection is joined
-        client.send('Connected to server!'.encode('ascii'))        #Connected and can starting
+        broadcast("{} joined!".format(nickname).encode('ascii'))   
+        client.send('Connected to server!'.encode('ascii'))        
 
-        # Start Handling Thread For Client
-        thread = threading.Thread(target=handle, args=(client,))   #This is because multiple client send multiple messages and so to avoid crashing we are handling with handler
-        thread.start()                                             #To work with threads you need start method or run method
+        thread = threading.Thread(target=handle, args=(client,))   
+        thread.start()                                             
 
-print("Server is started................,:) ")
+print("Server is started..............,:) ")
 receive()
+
+if __name__ == "__main__":
+    selectRoom = input("Select room 1: ")
+    if selectRoom == '1':
+        room1()
+    if selectRoom == '2':
+        room2()
+
+
